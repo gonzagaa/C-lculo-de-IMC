d1bada598e73662e7f76a14407da54be2718d782
“gonzagaa” <iigust4vo@gmail.com>
2023-11-29 09:43:34 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+import { AlertError } from './alertError.js'
+import { Modal } from './modal.js'
+import { IMC, NotANumber } from './utils.js'
+
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
+
+form.onsubmit = function(event) {
+    event.preventDefault()
+
+    const weight = inputWeight.value 
+    const height = inputHeight.value
+
+    const showAlertError = NotANumber(weight) || NotANumber(height)
+
+    if(showAlertError) {
+        AlertError.open()
+        return;
+    }
+
+    AlertError.close()
+
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerHTML = message
+    Modal.open()
+}
+
